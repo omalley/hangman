ed5627966d2cd18d215dd12cce3484a5ef306929
Hazen <hazen@GameAir.hsd1.ca.comcast.net>
2015-04-11 23:58:31 -0700

    working controller

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var pickedButtons = [UIButton]()
+    let stagePictures = [
+        UIImage(named: "gallows"),
+        UIImage(named: "head"),
+        UIImage(named: "body"),
+        UIImage(named: "left-arm"),
+        UIImage(named: "right-arm"),
+        UIImage(named: "left-leg"),
+        UIImage(named: "right-leg"),
+        UIImage(named: "hands"),
+        UIImage(named: "feet")
+    ]
+    var stage = 0
+    
+    @IBOutlet weak var gallows: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updatePicture(0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func updatePicture(newStage: Int) {
+        stage = newStage
+        gallows.image = stagePictures[stage]
+    }
+    
+    @IBAction func letterPushed(sender: UIButton) {
+        println("User pushed " + sender.currentTitle!)
+        sender.enabled = false
+        pickedButtons.append(sender)
+        if stage < stagePictures.count - 1 {
+            updatePicture(stage + 1)
+        }
+        //This function responds to buttons pushed
+    }
+    
+    @IBAction func newGame(sender: UIButton) {
+        println("New game")
+        for i in pickedButtons{
+            i.enabled = true
+        }
+        pickedButtons = []
+        updatePicture(0)
+        //this starts a new game
+    }
 }
 
