d7eb1a29d0debfeb07321124281a1577c7558799
Hazen <hazen@GameAir.hsd1.ca.comcast.net>
2015-05-23 17:13:06 -0700

    use new word list and greatly expanded vocabulary

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -30,6 +30,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        model.loadWords()
         model.pickWord()
         updatePicture()
     }
